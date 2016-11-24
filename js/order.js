8825f0cdb5686b0dd58dbd0da53029e5fa54ec26
David Shin <davidshin444@gmail.com>
2016-11-24 00:29:51 -0800

    Committing current changes

--- js/order.js
+++ js/order.js
@@ -1,6 +1,22 @@
 (function() {
     'use strict';
-    
+
     // Open and close the nav bar
     $('.button-collapse').sideNav();
+
+    // Make table elements
+    let table = getElementsByTagName('table');
+    let tbody = document.createElement('tbody');
+
+    // Event listener for cards
+    let cards = getElementsByClassName('className');
+    $$each(cards, function(card) {
+        card.addEventListener('click', function(event) {
+            if (event.target === card) {
+                console.log("this is the card", card);
+            }
+        });
+
+    })
+
 })();
